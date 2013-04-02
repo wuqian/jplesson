906b6fc4e1be57648dc41a41a4b0a8c23c8e0db3
wuqian <yer860519@gmail.com>
2013-04-02 20:47:37 +0800

    问答题修改功能完成

--- sites/all/modules/teacher/teacher_add_text_question.js
+++ sites/all/modules/teacher/teacher_add_text_question.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
     $('#chapters .form-select').hide();
+    //console.log(this.value);
+    //获取课程名称
+    var id = '#'+$('#edit-course').val();
+
+    $(id).show();
+    
     $('#edit-course').change(function(){
-        //console.log(this.value);
+        //console.log(this);
         $('#chapters .form-select').hide();
         //获取课程名称
         var id = '#'+this.value;
