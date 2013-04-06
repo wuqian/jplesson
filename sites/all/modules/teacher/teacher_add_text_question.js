7a0ab33560d4f280ef22b222d3afed4577eca390
wuqian <yer860519@gmail.com>
2013-04-06 20:39:07 +0800

    试题模块:添删改查 完成

--- sites/all/modules/teacher/teacher_add_text_question.js
+++ sites/all/modules/teacher/teacher_add_text_question.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
     $('#chapters .form-select').hide();
-    //console.log(this.value);
+
     //获取课程名称
     var id = '#'+$('#edit-course').val();
-
+    $(id).show();
+    var id = '#'+$('#edit-course').val() + '-wrapper';
     $(id).show();
     
     $('#edit-course').change(function(){
-        //console.log(this);
         $('#chapters .form-select').hide();
         //获取课程名称
         var id = '#'+this.value;
+        $(id).show();
+        var id = '#'+this.value + '-wrapper';
         $(id).show();
     });
 });
